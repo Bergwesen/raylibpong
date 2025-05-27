9fe54d474882d741b3f79ea7e57819e1acd4e5d0
a <b>
2025-05-27 17:44:14 +0200

    fast fertig

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,25 @@
 #include <iostream>
 #include <raylib.h>
 #include <raymath.h>
+#include <cmath>
+#define _USE_MATH_DEFINES
+
+double Maxwinkel = M_PI*0.5;
+int ballspeed = 2;
 
 Color yellow = {255, 255, 0, 255};
 Color blue = {0, 0, 255, 255};
 Color black = {0, 0, 0, 255};
+
+typedef enum Screens {Title ,Pause,Game} Screens;
 
 int Cellsize = 30;
 int Cellcount = 25;
 
 double oldtime = 0;
 
-bool eventrigger(double intervall) {
+
+/*bool eventrigger(double intervall) {
   double zeitspanne = GetTime() - oldtime;
   if (zeitspanne >= intervall) {
     oldtime = GetTime();
@@ -23,34 +31,38 @@ bool eventrigger(double intervall) {
   } else {
     return false;
   }
-}
+} */
 
 struct vec {
-  int x;
-  int y;
+  double x;
+  double y;
 };
 
-class Brick {
-public:
-  int x;
-  int y;
-  const int widthb = 10;
-  const int heightb = Cellsize * 2;
-  void Draw() { DrawRectangle(x, y, widthb, heightb, yellow); }
-};
 
 class Blop {
 public:
   struct vec cords;
   struct vec speed;
   int death = 0;
-  // warum kontte ich nicht hier drinnen das x und y definieren
 
   void spawn() {
-  cords.x = rand() % 500;
-  cords.y = rand() % 500;
-  speed.x = 4;
-  speed.y = rand() % 3;
+  cords.x = (rand() % 188)+200;
+  cords.y = 300+ (rand() % 188);
+  int direction = rand() % 2;
+  if(direction == 0){
+    direction = -1;
+  }else{
+    direction = 1;
+  } 
+  //speed.x=  ballspeed*(((rand()%10)+10)/10) *direction;
+  //speed.y=  ballspeed*(((rand()%10)+10)/10) ;
+  double einheitsx = (( (double) ((rand()) % 9)+1)) / 10;
+  double einheitsy = 1- std::pow(einheitsx,2);
+  std::cout << " Einheitstest " << einheitsx << " und " << einheitsy << std::endl;
+  //speed.x=  ballspeed*direction*einheitsx;
+  //speed.y=  ballspeed*einheitsy;
+  speed.x=  ballspeed*direction*1;
+  speed.y=  ballspeed*einheitsy*0;
   };
   int Draw() { 
     int olddeath = death;
@@ -63,30 +75,93 @@ public:
   }
 };
 
+class Brick
+{
+public:
+  int x;
+  int y;
+  const int widthb = 10;
+  const int heightb = Cellsize * 2;
+  void Draw() { DrawRectangle(x, y, widthb, heightb, yellow); }
+  void hit(Blop *kugel,int brick)
+  {
+    double relativy = kugel->cords.y - y - (heightb / 2);
+    double normrelativy = (relativy/(heightb/2));
+    double winkel = Maxwinkel * normrelativy;
+ //   std::cout << "rel " << relativy << " norm " <<normrelativy<< " winkel "<< winkel  <<std::endl;
+//    std::cout<< "old " << kugel->speed.x << " und " << kugel->speed.y << std::endl;
+    if( brick == 1){
+      kugel->speed.x = ballspeed * std::cos(winkel);
+      kugel->speed.y = -ballspeed * std::sin(winkel);
+     // std::cout << "new 1 " << kugel->speed.x << "test "<< std::cos(winkel) << " und " << kugel->speed.y << " " << winkel<< std::endl;
+    }else {
+      kugel->speed.x = -ballspeed * std::cos(winkel);
+      kugel->speed.y = ballspeed * std::sin(winkel);
+      //std::cout << "new 2 " << kugel->speed.x << "test "<< std::cos(winkel) << " und " << kugel->speed.y << " " << winkel<< std::endl;
+    }
+
+
+  }
+};
+
 int main() {
 
   // Create the window and OpenGL context
-  InitWindow(Cellsize * Cellcount, Cellsize * Cellcount, "Hello Raylib");
+  InitWindow(Cellsize * Cellcount, Cellsize * Cellcount, "Pong");
   SetTargetFPS(80);
   srand(time(0));
-  // game loop
   
-
+  //Werte Initialisierung
   std::string score1 = "0";
-  std::string score2 = "";
-  const int speedb = 5;
+  std::string score2 = "0";
+  const int speedb = 8;
+
   Brick brick = Brick();
   brick.x = 13;
   brick.y = 3;
+
   Brick brick2 = Brick();
   brick2.x = Cellcount * Cellsize - 23;
   brick2.y = 3;
 
   Blop kugel = Blop();
   kugel.spawn();
-  while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or
-                               // presses the Close button on the window
+
+  Screens Mainscreen = Title;
+  vec Titleposition;
+  Titleposition.x = 0;
+  Titleposition.y = 0;
+
+
+  int framecounter = 0;
+  //game loop
+  while (!WindowShouldClose()) 
+                              
   {
+    switch (Mainscreen)
+    {
+    case Title:
+      if(IsKeyDown(KEY_W)){
+        Titleposition.y = 1;
+
+      } else if(IsKeyDown(KEY_S)){
+        Titleposition.y = 0;
+      }
+      
+
+      if(Titleposition.y == 1 && IsKeyDown(KEY_ENTER)){
+        Mainscreen = Game;
+      }
+      break;
+    case Game:
+      {
+
+
+
+
+
+
+
     const vec cordspre = kugel.cords;
     // Brick move
     if (IsKeyDown(KEY_S) && (brick.y + brick.heightb) <= Cellcount * Cellsize) {
@@ -103,70 +178,63 @@ int main() {
       brick2.y = brick2.y - speedb;
     }
     // Kugel move
-    kugel.cords.x = kugel.cords.x + kugel.speed.x;
-    kugel.cords.y = kugel.cords.y + kugel.speed.y;
+    struct vec oldvecs;
+    oldvecs.x = kugel.cords.x;
+    oldvecs.y = kugel.cords.y;
 
-    // tunneling
-    // printf(" %d pre cords after cord %d \n", cordspre.x, kugel.cords.x);
+    //kugel.cords.x = kugel.cords.x + kugel.speed.x*kugel.schnell;
+    //kugel.cords.y = kugel.cords.y + kugel.speed.y*kugel.schnell;
 
-    int yhit2 = ((kugel.cords.y - cordspre.y) / (kugel.cords.x - cordspre.x)) *
-                    brick.x +
-                kugel.cords.y;
-    if ((yhit2 < brick2.y + brick2.heightb && yhit2 > brick2.y) &&
-        cordspre.x < brick2.x && kugel.cords.x > brick2.x) {
-      printf("HIT FOR SURE  %d y now %d y forsure\n", kugel.cords.y, yhit2);
-      kugel.cords.y = yhit2;
-      kugel.cords.x = brick2.x;
-    }
+    kugel.cords.x = kugel.cords.x + kugel.speed.x*ballspeed;
+    kugel.cords.y = kugel.cords.y + kugel.speed.y*ballspeed;
 
-    if ((kugel.cords.x < brick.x && cordspre.x > brick.x &&
-         cordspre.x < brick.x + brick.widthb) &&
-        ((kugel.cords.y > brick.y) &&
-         kugel.cords.y < brick.y + brick.heightb) &&
-        (cordspre.y < brick.y + kugel.speed.y ||
-         cordspre.y > brick.y - kugel.speed.y)) {
-      printf("Tunnel  ing \n");
-      int yhit = ((kugel.cords.y - cordspre.y) / (kugel.cords.x - cordspre.x)) *
-                 brick.x;
-      printf("yhit an kugel y %d  %d %d\n", kugel.cords.y, cordspre.y, yhit);
-      kugel.cords.y = kugel.cords.y + yhit;
+
+    int yhit = ((kugel.cords.y-oldvecs.y)/(oldvecs.x-kugel.cords.x) * brick.x) +kugel.cords.y;
+    int yhit2 = ((kugel.cords.y-oldvecs.y)/(-oldvecs.x+kugel.cords.x) * brick2.x) +kugel.cords.y;
+
+    if( kugel.cords.x <= brick.x && oldvecs.x >= brick.x)
+    {
+
+      if( yhit >= brick.y && yhit <= (brick.y+brick.heightb)){
+    //std::cout << yhit<< " und "<< brick.y << " u " << brick.y +brick.heightb << std::endl;
+      std::cout << " tunnel 1" << std::endl;
+      kugel.cords.y = yhit;
       kugel.cords.x = brick.x;
+      brick.hit(&kugel,1);
+      //kugel.speed.x = -kugel.speed.x ;
+      //kugel.speed.y = -kugel.speed.y ;
+      }else {
+        std::cout << "1  tunnel but no hit " << std::endl;
+      }
 
-      printf("yhit an 22  kugel y %d  %d %d\n", kugel.cords.y, kugel.cords.x,
-             yhit);
     }
 
-    if ((kugel.cords.x < brick2.x && cordspre.x > brick2.x &&
-         cordspre.x < brick2.x + brick2.widthb) &&
-        ((kugel.cords.y > brick2.y) &&
-         kugel.cords.y < brick2.y + brick2.heightb) &&
-        (cordspre.y < brick2.y + kugel.speed.y ||
-         cordspre.y > brick2.y - kugel.speed.y)) {
-      printf("Tunneling \n");
-      // int yhit = ((kugel.cords.y - cordspre.y) / (kugel.cords.x -
-      // cordspre.x)) *
-      //           brick2.x;
+    
+    if( kugel.cords.x >= brick2.x && oldvecs.x <= brick2.x)
+    { 
+      if( yhit >= brick2.y && yhit <= (brick2.y+brick2.heightb)){
+        std::cout << " tunnel " << std::endl;
+   // std::cout << yhit<< " und "<< brick.y << " u " << brick2.y +brick2.heightb << std::endl;
+      kugel.cords.y = yhit;
+      kugel.cords.x = brick2.x;
+        brick2.hit(&kugel,2);
+      //kugel.speed.x = -kugel.speed.x ;
+      //kugel.speed.y = kugel.speed.y ;
+      }else{
 
-      // kugel.cords.y = kugel.cords.y + yhit; kugel.cords.x = brick2.x;
+        std::cout << "  tunnel but no hit " << std::endl;
+      }
+
     }
 
-    //
+
+
+
     if (kugel.cords.x >= Cellcount * Cellsize || kugel.cords.x < 0) {
       printf(" Rechte Wand \n");
       kugel.speed.x = kugel.speed.x * -1;
       kugel.speed.y = kugel.speed.x * -1;
       kugel.death = 1;
-    }
-
-    if (kugel.cords.x == brick.x &&
-        kugel.cords.y <= (brick.heightb + brick.y) &&
-        kugel.cords.y >= brick.y) {
-      kugel.speed.x = kugel.speed.x * -1;
-    }
-    if (kugel.cords.x == brick2.x &&
-        kugel.cords.y <= (brick2.heightb + brick2.y) &&
-        kugel.cords.y >= brick2.y) {
-      kugel.speed.x = kugel.speed.x * -1;
     }
 
     if (kugel.cords.x < 0) {
@@ -182,9 +250,55 @@ int main() {
     if (kugel.cords.y < 0) {
       printf(" obere  Wand \n");
     }
+      framecounter = framecounter +1;
+      if(IsKeyDown(KEY_P) && framecounter > 50){
+        Mainscreen = Pause;
+        framecounter = 0;
+
+      }
+
+      }
+      break;
+    case Pause:
+         
+      framecounter = framecounter +1;
+      if(IsKeyDown(KEY_P) && framecounter > 10){
+        Mainscreen = Game;
+        framecounter = 0;
+      }
+
+    break;
+    }
 
     BeginDrawing();
 
+    switch (Mainscreen)
+    {
+    case Title:
+    {
+      DrawRectangle(0,0,Cellsize*Cellcount,Cellcount*Cellsize,BLACK);
+      if(Titleposition.y ==1 ){
+      DrawText("Play", Cellcount*Cellcount/2,Cellcount*Cellsize/2,40,RED);
+      DrawText("LOAD", Cellcount*Cellcount/2,Cellcount*Cellsize/2 +40,40,ORANGE);
+      }else {
+      DrawText("Play", Cellcount*Cellcount/2,Cellcount*Cellsize/2,40,ORANGE);
+      DrawText("LOAD", Cellcount*Cellcount/2,(Cellcount*Cellsize/2) +40,40,RED);
+
+      }
+      }
+      break;
+    case Pause:
+    {
+      DrawText("Paused",(Cellcount*Cellsize/2)-80,(Cellcount*Cellsize/2)-60,50,GRAY);
+
+
+
+    }
+      break;
+    case Game:
+    {
+
+    ClearBackground(black);
     DrawText(score1.c_str(), 200, 200, 40, WHITE);
     DrawText(score2.c_str(), 550, 200, 40, WHITE);
     int res = kugel.Draw();
@@ -193,11 +307,16 @@ int main() {
     } else if (res == 2){
       score2 = std::to_string(res-1 + std::stoi(score2));
     }
+
     brick.Draw();
     brick2.Draw();
     ClearBackground(black);
-
+    }    
+    break;
+    
+    }
     EndDrawing();
+
   }
 
   CloseWindow();
